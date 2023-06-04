4ac772f14c075fbb54853ce6afd6fe8ee7d805c6
fat83dotcom <fernando.quimic@gmail.com>
2023-06-04 03:03:40 -0300

    Modelo do banco de dados

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class DadoDiario(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    dia = models.DateTimeField(unique=True)
+    media_umidade = models.FloatField()
+    minimo_umidade = models.FloatField()
+    maximo_umidade = models.FloatField()
+    mediana_umidade = models.FloatField()
+    moda_umidade = models.FloatField()
+    media_pressao = models.FloatField()
+    minimo_pressao = models.FloatField()
+    maximo_pressao = models.FloatField()
+    mediana_pressao = models.FloatField()
+    moda_pressao = models.FloatField()
+    media_temp_int = models.FloatField()
+    minimo_temp_int = models.FloatField()
+    maximo_temp_int = models.FloatField()
+    mediana_temp_int = models.FloatField()
+    moda_temp_int = models.FloatField()
+    media_temp_ext = models.FloatField()
+    minimo_temp_ext = models.FloatField()
+    maximo_temp_ext = models.FloatField()
+    mediana_temp_ext = models.FloatField()
+    moda_temp_ext = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'dado_diario'
