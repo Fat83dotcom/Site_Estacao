5ef2dd2fe5b1e72eb81a61182176c364c5d3fce4
fat83dotcom <fernando.quimic@gmail.com>
2023-06-04 03:04:33 -0300

    Criação das function views basicas

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import DadoDiario
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index/index.html')
+
+
+def lista(request):
+    dado = DadoDiario.objects.order_by('-dia')
+    context = {
+        'dado': dado
+    }
+    return render(request, 'graficos/lista.html', context)
+
+
+def sobre(request):
+    return render(request, 'sobre/sobre.html')
