d1c3dd9b8ab12dfffee9eeb59fb3a9ee1cb836da
fat83dotcom <fernando.quimic@gmail.com>
2023-06-04 21:59:54 -0300

    Caminhos para as paginas do app

--- Core/urls.py
+++ Core/urls.py
@@ -3,6 +3,8 @@ from Core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lista/', views.lista, name='lista'),
+    path('registros/', views.registros, name='registros'),
+    path('registros/tabela', views.tabela, name='tabela'),
+    path('registros/graficos', views.graficos, name='graficos'),
     path('sobre/', views.sobre, name='sobre'),
 ]
