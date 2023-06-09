d372fb87df7f05f6c0364ff69b972d732e49643e
fat83dotcom <fernando.quimic@gmail.com>
2023-06-09 00:54:37 -0300

    Adição de novas views e mudança das chamdas de função para chamdas de classes.

--- Core/urls.py
+++ Core/urls.py
@@ -2,9 +2,22 @@ from django.urls import path
 from Core import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('registros/', views.registros, name='registros'),
-    path('registros/tabela', views.tabela, name='tabela'),
-    path('registros/graficos', views.graficos, name='graficos'),
-    path('sobre/', views.sobre, name='sobre'),
+    path('', views.PageIndexView.as_view(), name='index'),
+    path('registros/', views.PageMainRegisters.as_view(), name='registros'),
+    path('registros/tabela', views.PageMainTables.as_view(), name='tabela'),
+    path(
+        'registros/tabela/medias',
+        views.PagesTableMeanView.as_view(),
+        name='tabelamedias'
+    ),
+    # path(),
+    # path(),
+    # path(),
+    # path(),
+    path(
+        'registros/graficos',
+        views.PageMainGraphs.as_view(),
+        name='graficos'
+    ),
+    path('sobre/', views.PageAboutView.as_view(), name='sobre'),
 ]
