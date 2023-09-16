c5bfbe3adf0ef636938f70ae4d60847bbfba52ac
fat83dotcom <fernando.quimic@gmail.com>
2023-09-16 20:41:01 -0300

    Configuração da area administrativa para o gerenciamento de favicon.

--- Core/admin.py
+++ Core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DadoDiario, Pictures
+from .models import DadoDiario, Pictures, FavPicture
 
 
 @admin.register(DadoDiario)
@@ -10,3 +10,8 @@ class DadoDiarioAdmin(admin.ModelAdmin):
 @admin.register(Pictures)
 class PicturesAdmin(admin.ModelAdmin):
     ...
+
+
+@admin.register(FavPicture)
+class FavPictureAdmin(admin.ModelAdmin):
+    list_display = 'name', 'favIcon'
