2dfa3cfbd8d061a2e070b7c15e00eac062f17ad7
fat83dotcom <fernando.quimic@gmail.com>
2024-02-03 01:09:49 -0300

    Organização de código.

--- templates/static/javascript/fadeIndex.js
+++ templates/static/javascript/fadeIndex.js
@@ -1,23 +1,23 @@
 /*  Desenvolvido por: BrainStorm Tecnologia 
     Desenvolvedor: Fernando Mendes
-    todos os direitos reservados 2023 © BrainStorm Tecnologia
+    todos os direitos reservados 2024 © BrainStorm Tecnologia
 */
 
-window.addEventListener('load', () =>{
-    let contentA = document.getElementById('fade-info-A')
-    let contentB = document.getElementById('fade-info-B')
-    let contentC = document.getElementById('fade-graph-A')
-    let contentD = document.getElementById('fade-graph-B')
-    let flex = 'flex'
-    let value = '1'
-    contentA.style.display = flex
-    contentB.style.display = flex
-    contentC.style.display = flex
-    contentD.style.display = flex
-    setTimeout(() =>{
-      contentA.style.opacity = value
-      contentB.style.opacity = value
-      contentC.style.opacity = value
-      contentD.style.opacity = value
-    }, 500)
-  })
+window.addEventListener('load', () => {
+  let contentA = document.getElementById('fade-info-A')
+  let contentB = document.getElementById('fade-info-B')
+  let contentC = document.getElementById('fade-graph-A')
+  let contentD = document.getElementById('fade-graph-B')
+  let flex = 'flex'
+  let value = '1'
+  contentA.style.display = flex
+  contentB.style.display = flex
+  contentC.style.display = flex
+  contentD.style.display = flex
+  setTimeout(() => {
+    contentA.style.opacity = value
+    contentB.style.opacity = value
+    contentC.style.opacity = value
+    contentD.style.opacity = value
+  }, 500)
+})
